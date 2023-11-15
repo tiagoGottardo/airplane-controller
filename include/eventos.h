43b93eda0354c145949cd6664c18fb5ec715fe91
tiagoGott <tiago.panizio@hotmail.com>
2023-11-14 21:32:11 -0300

    insere e decola iniciados

---
+++ include/eventos.h
@@ -0,0 +1,3 @@
+void InicializaPistas(int quantidadeDePistas);
+void SpawnaAviao(char* idPista, int codigo, char* modelo, char* cidadeDestino, float distancia, int tempoEstimadoDeVoo);
+void Decola(int numPista);
--- src/main.c
+++ src/main.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "../include/parser.h"
 #include "../include/listas.h"
+#include "../include/eventos.h"
+
+Aviao** pista = NULL;
+Aviao* ceu = NULL;
+Aviao* destino = NULL;
 
 int main() {
-  Aviao *pista = NULL;
+  srand(((unsigned int)time(NULL)));
 
-  Aviao *voando = NULL;
   // LeArquivo();
 
-  AdicionaAviao(&pista, 2);
-  AdicionaAviao(&pista, 4);
-  AdicionaAviao(&pista, 6);
+  InicializaPistas(2);
 
-  Aviao* retirado = RetiraNoInicio(&pista); 
-  Indexa(retirado, &voando);
+  SpawnaAviao("f1", 1, "A220", "SP", 409, 50);
+  SpawnaAviao("f1", 2, "A320", "Brasilia", 409, 50);
+  // SpawnaAviao("f2", 2, "A320", "Brasilia", 409, 50);
+  SpawnaAviao("f2", 3, "A420", "New York", 409, 50);
 
-  retirado = RetiraNoInicio(&pista); 
-  Indexa(retirado, &voando);
+  Decola(1);
+  Decola(1);
+  Decola(2);
   
-  MostraPista(pista);
+  printf("Ceu Antes:\n");
+  MostraPista(ceu);
 
-  MostraPista(voando);
-
+  // printf("Pista 1:\n");
+  // MostraPista(pista[0]);
+  
+  // printf("Pista 2:\n");
+  // MostraPista(pista[1]);
+  
+  printf("Ceu:\n");
+  MostraPista(ceu);
 
   return 0;
 }
