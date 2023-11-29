7a4af507f8b0e95166b32d78be77abc8249e4b5b
tiagoGott <tiago.panizio@hotmail.com>
2023-11-29 14:00:43 -0300

    interface atualizada

--- src/interface.c
+++ src/interface.c
@@ -1,52 +1,71 @@
 #include "../include/interface.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void printajuda() {
-    printf("Ajuda");
+void MostrarComandos() {
+    printf("\n");
+    printf(" - reiniciar\n");
+    printf(" - voltar\n");
+    printf(" - pistas(quantidade)\n");
+    printf(" - insere_aviao(pista, codigo, modelo, destino, distancia, tempo de voo)\n");
+    printf(" - decolar(pista)\n");
+    printf(" - tempestade(minuto)\n");
+    printf(" - turbulencia(minuto)\n");
+    printf(" - neblina(minuto)\n");
+    printf(" - iniciasimulacao(minutos)\n");
+    printf("\n");
     return;
 }
 
-void inputcli() {
+void Cli() {
   char input[128];
 
-  printf("Digite os inputs\n");
-  printf("Digite ? para ser listado os inputs possíveis\n");
-
-  fflush(stdin);
+  printf("========================== Aeroporto ==========================\n");
+  printf("\nDigite 'ajuda' para conhecer todos os comandos.\n\n");
 
   while (1) {
     int numElementos;
     char **elementos;
 
-    fgets(input, sizeof(input), stdin);
+    printf("Comando: ");
+    scanf(" %[^\n]", input);
 
-    if (!strcmp(input, "ajuda\n")) {
-        printajuda();
-    } else if (!strcmp(input, "reiniciar\n")) {
-      inputcli();
-      return;
-    } else if(strcmp(input, "\n")){
+    if (!strcmp(input, "ajuda")) {
+        MostrarComandos();
+    } else if (!strcmp(input, "reiniciar")) {
+      system("clear");
+      Finaliza();
+      Cli(); return;
+    } else if (!strcmp(input, "voltar")) {
+      system("clear");
+      Finaliza();
+      IniciaInterface(); return;
+    } else if (strcmp(input, "\n")) {
       SeparaElementos(input, &elementos, &numElementos);
       ChamaFuncoes(elementos, numElementos);
     }
   }
+
 }
 
-void interface() {
-  int input;
-  printf("Simulador de controle de tráfego aereo - INTERFACE\n");
-  printf("1- Usar arquivo como entrada\n");
-  printf("2- Digitar entrada\n");
-  scanf("%i", &input);
-  initLogFile();
+void IniciaInterface() {
+  char input[128];
+  
+  while(1) {
 
-  if (input == 1) {
-    LeArquivo();
-  } else if (input == 2) {
-    inputcli();
-  } else {
-    printf("ERRO: entrada invalida");
-    return;
+    printf("================== Simulador de tráfego aéreo =================\n\n");
+    printf("Importar um (a)rquivo | Digitar comandos (m)anualmente [a, m]:");
+    
+    scanf(" %[^\n]", input);  
+    initLogFile();
+
+    system("clear");
+  
+    switch(input[0]) {
+      case 'a': LeArquivo(); break;
+      case 'm': Cli(); break;
+    }
+
   }
 }
